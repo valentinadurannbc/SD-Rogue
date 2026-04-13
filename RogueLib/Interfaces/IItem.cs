b75421844b948a58300e3c88b5330da61af2548c
Teixeira Renato <reteixeirajunior@gmail.com>
2026-04-13 14:31:10 -0300

    interface IItem implemented

---
+++ RogueLib/Interfaces/IItem.cs
@@ -0,0 +1,12 @@
+﻿using RogueLib.Utilities;
+
+namespace RogueLib.Dungeon;
+
+public interface IItem : IDrawable
+{
+    Vector2 Pos { get; }
+    string Name { get; }
+    bool IsWeapon { get; }
+    int StrBonus { get; }
+    void Use(Player player);
+}
