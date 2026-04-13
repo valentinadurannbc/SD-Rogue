c9a75a93c24994278763dc4c1d32b8bcd222befb
Teixeira Renato <reteixeirajunior@gmail.com>
2026-04-13 14:52:31 -0300

    Key created

---
+++ RougeLikeGame/Items/Key.cs
@@ -0,0 +1,22 @@
+﻿using RogueLib.Dungeon;
+using RogueLib.Utilities;
+
+namespace RlGameNS;
+
+public class Key : IItem
+{
+    public Vector2 Pos { get; }
+    public string Name => "Key";
+    public bool IsWeapon => false;
+    public int StrBonus => 0;
+
+    public Key(Vector2 pos)
+    {
+        Pos = pos;
+    }
+
+    public void Draw(IRenderWindow disp) =>
+       disp.Draw('k', Pos, ConsoleColor.Yellow);
+
+    public void Use(Player player) { }
+}
