6e16855615213a4fd62f0b8918d885695f0883e8
Teixeira Renato <reteixeirajunior@gmail.com>
2026-04-13 14:45:47 -0300

    Folder Items and class Apple created

---
+++ RougeLikeGame/Items/Apple.cs
@@ -0,0 +1,23 @@
+﻿using RogueLib.Dungeon;
+using RogueLib.Utilities;
+
+namespace RlGameNS;
+
+public class Apple : IItem
+{
+    public Vector2 Pos { get; }
+    public string Name => "Apple";
+    public bool IsWeapon => false;
+    public int StrBonus => 0;
+
+    public Apple(Vector2 pos)
+    {
+        Pos = pos;
+    }
+
+    public void Draw(IRenderWindow disp) =>
+       disp.Draw('$', Pos, ConsoleColor.Red);
+
+    public void Use(Player player) =>
+       player.Heal(5);
+}
