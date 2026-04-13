e09ded788d82f003bdb7eb0f787b8045c09b7251
Teixeira Renato <reteixeirajunior@gmail.com>
2026-04-13 14:55:12 -0300

    Death Scene created

---
+++ RougeLikeGame/Levels/DeathScene.cs
@@ -0,0 +1,31 @@
+﻿using RogueLib;
+using RogueLib.Dungeon;
+using RogueLib.Engine;
+using RogueLib.Utilities;
+
+namespace RlGameNS;
+
+public class DeathScene : Scene
+{
+    public DeathScene(Player player, Game game)
+    {
+        _player = player;
+        _game = game;
+        RegisterCommand(ConsoleKey.Q, "quit");
+        RegisterCommand(ConsoleKey.Enter, "quit");
+        RegisterCommand(ConsoleKey.Escape, "quit");
+    }
+
+    public override void Draw(IRenderWindow disp)
+    {
+        disp.Draw(DungeonConfig.RIP, ConsoleColor.White);
+        disp.Draw("Press Q or Enter to exit", new Vector2(0, 24), ConsoleColor.Yellow);
+    }
+
+    public override void Update() { }
+
+    public override void DoCommand(Command command)
+    {
+        if (command.Name == "quit") QuitLevel();
+    }
+}
