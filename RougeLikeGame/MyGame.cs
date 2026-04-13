a2d4e41a17163af5b7b045b1d41a6795192f33d5
Teixeira Renato <reteixeirajunior@gmail.com>
2026-04-13 15:12:12 -0300

    My Game updated

--- RougeLikeGame/MyGame.cs
+++ RougeLikeGame/MyGame.cs
@@ -5,39 +5,51 @@ using RogueLib.Utilities;
 namespace RlGameNS;
 
 
-public class MyGame : Game {
-   
-   private void init() {
-      // To create a new game just need to 
-      // 'inject' an IRenderWindow to draw the game one
-      // 'inject' a Player, the player lives outside or the Scene's because the 
-      // player visits all the scenes and takes their inventory with them. 
-      // you must load the first leveel, and your level or your game must manage 
-      // the level switching. 
-      
-      _window       = new ScreenBuff();
-      _player       = new Rogue();
-      _currentLevel = new Level(_player, map1, this);
-      
-   }
+public class MyGame : Game
+{
 
-   public MyGame() {
-      // init level on construction 
-      init();
-   }
+    private void init()
+    {
+        // To create a new game just need to 
+        // 'inject' an IRenderWindow to draw the game one
+        // 'inject' a Player, the player lives outside or the Scene's because the 
+        // player visits all the scenes and takes their inventory with them. 
+        // you must load the first leveel, and your level or your game must manage 
+        // the level switching. 
 
-   
-   // ----------------------------------------------------------------
-   // string to use as the backgound on our first level
-   // ----------------------------------------------------------------
+        _window = new ScreenBuff(width, height);
+        _player = new Rogue();
 
-   public const string map1 = 
-      """
+        var level1 = new Level(_player, map1, this);
+        var level2 = new Level2(_player, map2, this);
+
+        level1.Linked = level2;
+        level2.Linked = level1;
+
+        level1.InitPlayer(new Vector2(4, 12));
+
+        _currentLevel = level1;
+
+    }
+
+    public MyGame()
+    {
+        // init level on construction 
+        init();
+    }
+
+
+    // ----------------------------------------------------------------
+    // string to use as the backgound on our first level
+    // ----------------------------------------------------------------
+
+    public const string map1 =
+       """
 
                ┌──────┐          ┌─────────────┐
                │......│        ##+.............│            ┌───────┐
-               │......│        # │.............+##          │.......│
-               │......+######### └──────────+──┘ ###########+.......│
+               │......│        # │.............+##          │......%│
+               │......+######### └──────────+──┘ ###########+.......│                                                    
                │......│                     #               └───────┘
                └──+───┘                     #
            ########                 #########
@@ -46,7 +58,7 @@ public class MyGame : Game {
       │.....+#####################+.........│              │..................│
       │.....│                     │.........│              │..................│
       │.....│                     │.........│              │..................│
-      │.....│                     │.........+##############+..................│
+      │....>│                     │.........+##############+..................│
       └─+───┘                     └───+─────┘              └───────────────+──┘
         #                             #                                    #
         ######               ┌────────+──────────────┐                     #
@@ -58,5 +70,32 @@ public class MyGame : Game {
              ###############
              
              
+      """;
+
+    public const string map2 =
+         """
+
+                           ┌─────────────┐
+           ┌──────┐      ##+.............│                  ┌───────┐
+           │......│  ##### │.............│     #############+......%│
+           │......│###     │.............│     #            │.......│
+           │......│        │.............│     #            └───────┘
+           └──+───┘        │.............+######
+             ##            │.............│     #
+             #             │.............│     #
+             #             │.............│     # ┌──────────┐
+             #             └───+─────────┘     ##+..........│
+             #                 #                 │..........│
+           ###             #####                 │..........│
+             ###     #######                     │..........│
+             # #######     #                     └────+─────┘
+           ###             #                          #
+           #             ┌─+────────┐                 #
+      ┌────+─────┐       │..........│                 #
+      │..........│       │..........+##################
+      │..........│       └──────────┘
+      │..........│
+      └──────────┘
+
       """;
 }
