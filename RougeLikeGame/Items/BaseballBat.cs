68a972a6edd9b459f73f5ee26717f82a22fcf0b1
Teixeira Renato <reteixeirajunior@gmail.com>
2026-04-13 14:50:06 -0300

    BaseballBat created

---
+++ RougeLikeGame/Items/BaseballBat.cs
@@ -0,0 +1,22 @@
+﻿using RogueLib.Dungeon;
+using RogueLib.Utilities;
+
+namespace RlGameNS;
+
+public class BaseballBat : IItem
+{
+    public Vector2 Pos { get; }
+    public string Name => "Baseball Bat";
+    public bool IsWeapon => true;
+    public int StrBonus => 3;
+
+    public BaseballBat(Vector2 pos)
+    {
+        Pos = pos;
+    }
+
+    public void Draw(IRenderWindow disp) =>
+       disp.Draw('/', Pos, ConsoleColor.DarkYellow);
+
+    public void Use(Player player) { }
+}
