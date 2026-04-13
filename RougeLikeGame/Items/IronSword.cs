1be6ecf267c31d6fc988b61828fd9986843f0345
Teixeira Renato <reteixeirajunior@gmail.com>
2026-04-13 14:48:08 -0300

    IronSword created

---
+++ RougeLikeGame/Items/IronSword.cs
@@ -0,0 +1,22 @@
+﻿using RogueLib.Dungeon;
+using RogueLib.Utilities;
+
+namespace RlGameNS;
+
+public class IronSword : IItem
+{
+    public Vector2 Pos { get; }
+    public string Name => "Iron Sword";
+    public bool IsWeapon => true;
+    public int StrBonus => 6;
+
+    public IronSword(Vector2 pos)
+    {
+        Pos = pos;
+    }
+
+    public void Draw(IRenderWindow disp) =>
+       disp.Draw('T', Pos, ConsoleColor.Cyan);
+
+    public void Use(Player player) { }
+}
